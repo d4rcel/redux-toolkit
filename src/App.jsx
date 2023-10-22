dd8bcd89c8dbddc7c771449395dc2d8ad5f9aa90
d4rcel <daraola05@gmail.com>
2023-10-22 12:15:48 +0100

    Build

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from 'react';
+import Modal from './components/Modal';
+
+function App() {
+  const {cartItems} = useSelector(store => store.cart)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    // Voir avec useCallBack
+    dispatch(calculateTotals())
+  }, [cartItems, dispatch])
+  return <>
+    <main>
+      <Modal/>
+      <Navbar/>
+      <CartContainer/>
+    </main>
+  </>;
+}
+export default App;
